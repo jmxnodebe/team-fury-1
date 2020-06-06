2b9af5d7424559164a83007af257301bbc89961c
jmxnodebe <53544100+jmxnodebe@users.noreply.github.com>
2020-06-06 07:46:41 +0100

    Create donttestnodii.php
    stage 1 task modified

---
+++ scripts/donttestnodii.php
@@ -0,0 +1,10 @@
+<?php
+
+            $name = 'Maximillian Nodebe';
+            $id = 'HNG-06278';
+            $language = 'PHP';
+            $email = 'mxximillianjay@gmail.com';
+
+            echo  "Hello World, this is $name with HNGi7 ID $id and email $email using $language for stage 2 task";
+
+?> 
