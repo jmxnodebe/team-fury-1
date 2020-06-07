5edbe3ac586b266088bacd52f0a255c4ea180f7e
jmxnodebe <53544100+jmxnodebe@users.noreply.github.com>
2020-06-07 17:01:23 +0100

    Create donttestnodi.php

---
+++ donttestnodi.php
@@ -0,0 +1,11 @@
+<?php
+
+            $name = 'Maximillian Nodebe';
+            $id = 'HNG-06278';
+            $language = 'PHP';
+             
+            $output = "Hello World, this is $name with HNGi7 ID $id using $language for stage 2 task";
+            
+            echo $output;
+
+?> 
